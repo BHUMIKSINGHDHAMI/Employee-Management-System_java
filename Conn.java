0342c9240b79abda0d14d75a7b5d261325572d14
BHUMIKSINGHDHAMI <dhamibhumiksingh@gmail.com>
2026-04-28 22:52:02 +0530

    Create Conn.java

---
+++ Conn.java
@@ -0,0 +1,25 @@
+package employee.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    Connection c; // interface
+    Statement s;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            c = DriverManager.getConnection(
+    "jdbc:mysql://localhost:3306/employeemanagementsystem?useSSL=false&serverTimezone=UTC",
+    "root",
+    "bhumik@07"
+);
+
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
